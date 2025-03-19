4c9e419ff0f0773f92402da6478c7e7780a9f6f8
nasenalm <nasenealm@gmail.com>
2025-03-19 14:44:54 -0600

    hb

---
+++ ProjectSourceCode/index.js
@@ -0,0 +1,54 @@
+// Import dependencies
+const express = require('express'); // To build an application server or API
+const app = express();
+const handlebars = require('express-handlebars');
+const Handlebars = require('handlebars');
+const path = require('path');
+const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
+const bodyParser = require('body-parser');
+const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
+const bcrypt = require('bcryptjs'); //  To hash passwords
+const axios = require('axios'); // To make HTTP requests from our server.
+
+
+
+
+
+/// Handlebars config
+const hbs = handlebars.create({
+    extname: 'hbs',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+  });
+
+
+// Register `hbs` as our view engine using its bound `engine()` function.
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
+
+
+
+
+
+
+/// Endpoint Config ///
+
+// home
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+    // render home page when complete
+});
+
+
+/// End Endpoint Config ///
+
+
+
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
