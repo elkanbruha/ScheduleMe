0c5cc87138cfc14a265e417e4cb2cd40ee50a1d3
Prajinkumar Suresh Kannan <prsu4616@colorado.edu>
2025-04-02 14:56:22 -0600

    added the post function for login

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -22,6 +22,29 @@ const hbs = handlebars.create({
   });
 
 
+// database configuration
+const dbConfig = {
+    host: 'db', // the database server
+    port: 5432, // the database port
+    database: process.env.POSTGRES_DB, // the database name
+    user: process.env.POSTGRES_USER, // the user account to connect with
+    password: process.env.POSTGRES_PASSWORD, // the password of the user account
+  };
+  
+  const db = pgp(dbConfig);
+  
+  // test your database
+  db.connect()
+    .then(obj => {
+      console.log('Database connection successful'); // you can view this message in the docker compose logs
+      obj.done(); // success, release the connection;
+    })
+    .catch(error => {
+      console.log('ERROR:', error.message || error);
+    });
+
+
+
 // Register `hbs` as our view engine using its bound `engine()` function.
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
@@ -46,9 +69,41 @@ app.get('/register', (req, res) => {
     res.render('pages/Register'); 
 });
 
+// app.get('/login', (req, res) => {
+//     res.render('pages/login'); 
+// });
 app.get('/login', (req, res) => {
-    res.render('pages/login'); 
-});
+    //do something
+    res.render('pages/login');
+  });
+
+app.post('/login', async (req, res) => {
+    // To-DO: Query the 'users' table to find the user with the username from the request
+    const user = await db.oneOrNone(`SELECT * FROM Users WHERE user_id = $1`, [req.body.User_id]);
+    // check if password from request matches with password in DB
+    console.log(user);
+    if (!user) {
+      console.log('not found');
+      res.redirect('/login');
+    }
+    else {
+      const match = await bcrypt.compare(req.body.password, user.password);
+      if (!match) {
+
+        res.redirect('/login');
+        console.log('Invalid username or password');
+
+      } 
+      else {
+        //save user details in session like in lab 7
+        req.session.user = user;
+        req.session.save();
+        res.redirect('/home');
+      }}
+  });
+
+
+
 
 app.get('/logout', (req, res) => {
     res.render('pages/logout'); 
