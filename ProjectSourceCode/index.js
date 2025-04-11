a036d66a6f1df3e1c73156a4e9e700529426b940
Lucas Williamson <lucas.williamson@colorado.edu>
2025-04-11 14:09:05 -0600

    help GET added

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -245,6 +245,10 @@ app.get('/calendar', (req, res) => {
     res.render('pages/Calendar'); 
 });
 
+app.get('/help', (req, res) => {
+  res.render('pages/help'); 
+});
+
 
 app.post('/appointments', async (req, res) => {
   // Check if user is logged in
