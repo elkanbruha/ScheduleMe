21c6163f5d123f4654d73ab69f7d42b6d1d0c5b0
Lucas Williamson <lucas.williamson@colorado.edu>
2025-04-11 11:34:00 -0600

    get for help page

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -211,6 +211,11 @@ app.get('/calendar', (req, res) => {
     res.render('pages/Calendar'); 
 });
 
+app.get('/help', (req, res) => {
+  res.render('pages/help'); 
+});
+
+
 app.post('/appointments', async (req, res) => {
   // Check if user is logged in
   if (!req.session.user || !req.session.userType) {
